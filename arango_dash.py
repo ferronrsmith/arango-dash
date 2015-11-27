fefab21e37f7957f3eed24a1a7cc9cdad2c36a30
ferron <ferronrsmith@gmail.com>
2015-11-27 04:07:58 -0400

    rename folder

--- arango_dash.py
+++ arango_dash.py
@@ -3,29 +3,35 @@
 import os
 import re
 import sqlite3
-
 from bs4 import BeautifulSoup
 
-db = sqlite3.connect('arangodb.docset/Contents/Resources/docSet.dsidx')
+db = sqlite3.connect('ArangoDB.docset/Contents/Resources/docSet.dsidx')
 cur = db.cursor()
 
-try: cur.execute('DROP TABLE searchIndex;')
-except: pass
+folder = 'docs.arangodb.com'
+
+try:
+    cur.execute('DROP TABLE searchIndex;')
+except:
+    pass
 cur.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
 cur.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
 
-docpath = 'arangodb.docset/Contents/Resources/Documents/docs.arangodb.com'
+docpath = 'ArangoDB.docset/Contents/Resources/Documents/' + folder
 
-page = open(os.path.join(docpath,'index.html')).read()
+page = open(os.path.join(docpath, 'index.html')).read()
 soup = BeautifulSoup(page)
 
 any = re.compile('.*')
-for tag in soup.find_all('a', {'href':any}):
+for tag in soup.find_all('a', {'href': any}):
     name = tag.text.strip()
+    name = re.sub("\n\s+", " ", name)
     if len(name) > 0:
-        path = tag.attrs['href'].strip()
+        href = tag.attrs['href']
+        path = re.sub("(\./)", "", href)
+        path = folder + "/" + path
         if path.split('#')[0] not in ('index.html', 'biblio.html', 'bookindex.html'):
-            cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'func', path))
+            cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, 'Guide', path))
             print 'name: %s, path: %s' % (name, path)
 
 db.commit()
